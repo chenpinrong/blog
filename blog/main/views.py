dd9cabeb63676af6ff5e523b805859b7e062d6e5
pinrong <pinrong@m516-HP-EliteDesk-800-G1-TWR>
2017-12-13 14:27:42 +0800

    Chapter 7 finished

--- blog/main/views.py
+++ blog/main/views.py
@@ -1,3 +1,21 @@
+import datetime
 from django.shortcuts import render
+
+
+
+def main(request):
+    '''
+    Render the main page
+    '''
+    context = {'like':'Django 很棒','now':datetime.datetime.now()}
+    return render(request, 'main/main.html', context)
+
+
+
+def about(request):
+    '''
+    Render the about page
+    '''
+    return render(request, 'main/about.html')
 
 # Create your views here.
